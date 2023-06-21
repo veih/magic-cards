815bce0da6b5b0803fab18e10565051f35d0d398
Marcelo Evangelista <tancker@hotmail.com>
2023-06-20 22:06:17 -0300

    ajustes

---
+++ magiccards/src/app/components/Form.tsx
@@ -0,0 +1,22 @@
+
+'use client'
+import Form from 'react-bootstrap/Form';
+
+function FormTextExample() {
+  return (
+    <>
+      <Form.Label htmlFor="inputPassword5" className='card'>Password</Form.Label>
+      <Form.Control
+        type="password"
+        id="inputPassword5"
+        aria-describedby="passwordHelpBlock"
+      />
+      <Form.Text id="passwordHelpBlock" className='card' muted>
+        Your password must be 8-20 characters long, contain letters and numbers,
+        and must not contain spaces, special characters, or emoji.
+      </Form.Text>
+    </>
+  );
+}
+
+export default FormTextExample;
