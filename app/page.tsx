01fb2e6867e6639af857503f291194d2e3e3d647
Marcelo evangelista de Oliveira <tancker@hotmail.com>
2023-07-11 18:15:56 -0300

    compnente loading

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { FormEvent, useState } from "react";
+import { Loading } from "./components/loading";
 
 type WSResults = {
   price: string;
@@ -33,6 +34,7 @@ export default function Home() {
         "Content-Type": "application/json",
       },
     });
+    
     const { products } = await ligaMagic.json();
 
     console.log(products)
@@ -60,85 +62,7 @@ export default function Home() {
       </form>
 
       {isLoading &&
-        <svg
-          version="1.1"
-          id="L1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 100 100"
-          enableBackground="new 0 0 100 100"
-          xmlSpace="preserve"
-        >
-          <circle fill="none" stroke="#fff" stroke-width="6" stroke-miterlimit="15" stroke-dasharray="14.2472,14.2472" cx="50" cy="50" r="47" >
-            <animateTransform
-              attributeName="transform"
-              attributeType="XML"
-              type="rotate"
-              dur="5s"
-              from="0 50 50"
-              to="360 50 50"
-              repeatCount="indefinite" />
-          </circle>
-          <circle fill="none" stroke="#fff" stroke-width="1" stroke-miterlimit="10" stroke-dasharray="10,10" cx="50" cy="50" r="39">
-            <animateTransform
-              attributeName="transform"
-              attributeType="XML"
-              type="rotate"
-              dur="5s"
-              from="0 50 50"
-              to="-360 50 50"
-              repeatCount="indefinite" />
-          </circle>
-          <g fill="#fff">
-            <rect x="30" y="35" width="5" height="30">
-              <animateTransform
-                attributeName="transform"
-                dur="1s"
-                type="translate"
-                values="0 5 ; 0 -5; 0 5"
-                repeatCount="indefinite"
-                begin="0.1" />
-            </rect>
-            <rect x="40" y="35" width="5" height="30" >
-              <animateTransform
-                attributeName="transform"
-                dur="1s"
-                type="translate"
-                values="0 5 ; 0 -5; 0 5"
-                repeatCount="indefinite"
-                begin="0.2" />
-            </rect>
-            <rect x="50" y="35" width="5" height="30" >
-              <animateTransform
-                attributeName="transform"
-                dur="1s"
-                type="translate"
-                values="0 5 ; 0 -5; 0 5"
-                repeatCount="indefinite"
-                begin="0.3" />
-            </rect>
-            <rect x="60" y="35" width="5" height="30" >
-              <animateTransform
-                attributeName="transform"
-                dur="1s"
-                type="translate"
-                values="0 5 ; 0 -5; 0 5"
-                repeatCount="indefinite"
-                begin="0.4" />
-            </rect>
-            <rect x="70" y="35" width="5" height="30" >
-              <animateTransform
-                attributeName="transform"
-                dur="1s"
-                type="translate"
-                values="0 5 ; 0 -5; 0 5"
-                repeatCount="indefinite"
-                begin="0.5" />
-            </rect>
-          </g>
-        </svg>
+        <Loading />
       }
 
       <div className="grid grid-cols-3 gap-4">
