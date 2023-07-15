165386795052366f9eb45217eef77ef5ddb4e022
Marcelo evangelista de Oliveira <tancker@hotmail.com>
2023-07-15 14:05:33 -0300

    ajuste nas cores

--- app/page.tsx
+++ app/page.tsx
@@ -14,29 +14,13 @@ type WSResults = {
 
 export default function Home() {
   const [searchPrompt, setSearchPrompt] = useState("");
-  const [searchPromptNext, setSearchPromptNext] = useState(1);
   const [searchResults, setSearchResults] = useState<WSResults[]>([]);
-  const [searchResultsNext, setSearchResultsNext] = useState<WSResults[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const loadMoreRef = useRef(null);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-
-      if (scrollTop + clientHeight >= scrollHeight - 20 && searchResultsNext.length > 0 && !isLoading) {
-        handleLoadMore();
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [searchResultsNext, isLoading]);
 
   const fetchData = async (searchParam: string, nextPage?: number) => {
     setIsLoading(true);
 
-    const response = await fetch("/searchprodligamagic", {
+    const response = await fetch("/searchproducts", {
       method: "POST",
       body: JSON.stringify({
         searchPrompt: searchParam,
@@ -54,28 +38,12 @@ export default function Home() {
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setSearchResults([]);
-    setSearchPromptNext(1);
 
     const products = await fetchData(searchPrompt);
     setSearchResults(products);
     setSearchPrompt("");
     setIsLoading(false);
   };
-
-  const handleLoadMore = async () => {
-    setIsLoading(true);
-
-    const productsNext = await fetchData(searchPrompt, searchPromptNext);
-    setSearchResults((prevResults) => [...prevResults, ...productsNext]);
-    setSearchPromptNext((prevNext) => prevNext + 1);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    setSearchResults((prevResults) => [...prevResults, ...searchResultsNext]);
-    setSearchResultsNext([]);
-    setIsLoading(false);
-  }, []);
 
   return (
     <main className="max-w-5xl mx-auto flex flex-col mt-2 justify-center">
@@ -97,76 +65,74 @@ export default function Home() {
 
       {isLoading && <Loading />}
 
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-1">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-2">
         {searchResults.map((prod, i) => (
-          <div key={i} className="bg-gray-800 bg-opacity-50 p-4 rounded-lg grid grid-cols-2 gap-2 justify-center">
-            <div className="relative h-100 w-60 mt-2" >
-              <Image
-                src={prod.imageUrl.startsWith("//") ? "https:" + prod.imageUrl : prod.imageUrl}
-                alt={prod.title}
-                fill
-                className="rounded-lg"
-              />
-            </div>
-            <div className="flex flex-col">
-              <p className="text-white text-center mt-1">Liga Magic</p>
-              <table className="mt-1">
-                <tbody>
-                  <tr>
-                    <td className="text-white">Preço mínimo</td>
-                    <td className="text-white text-right">{prod.priceMin}</td>
-                  </tr>
-                  <tr>
-                    <td className="text-white">Preço médio</td>
-                    <td className="text-white text-right">{prod.priceMed}</td>
-                  </tr>
-                  <tr>
-                    <td className="text-white">Preço máximo</td>
-                    <td className="text-white text-right">{prod.priceMax}</td>
-                  </tr>
-                </tbody>
-              </table>
-              <p className="text-white text-center mt-1">Star City</p>
-              <table className="mt-1">
-                <tbody>
-                  <tr>
-                    <td className="text-white">Preço mínimo</td>
-                    <td className="text-white text-right">{prod.priceMin}</td>
-                  </tr>
-                  <tr>
-                    <td className="text-white">Preço médio</td>
-                    <td className="text-white text-right">{prod.priceMed}</td>
-                  </tr>
-                  <tr>
-                    <td className="text-white">Preço máximo</td>
-                    <td className="text-white text-right">{prod.priceMax}</td>
-                  </tr>
-                </tbody>
-              </table>
-              <p className="text-white text-center mt-1">Magic Domen</p>
-              <table className="mt-1">
-                <tbody>
-                  <tr>
-                    <td className="text-white">Preço mínimo</td>
-                    <td className="text-white text-right">{prod.priceMin}</td>
-                  </tr>
-                  <tr>
-                    <td className="text-white">Preço médio</td>
-                    <td className="text-white text-right">{prod.priceMed}</td>
-                  </tr>
-                  <tr>
-                    <td className="text-white">Preço máximo</td>
-                    <td className="text-white text-right">{prod.priceMax}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-
+          <div key={i} className="bg-gray-700 p-5 rounded-lg grid grid-cols-2 gap-2 justify-center">
+          <div className="relative h-100 w-60 mt-2">
+            <Image
+              src={prod.imageUrl.startsWith("//") ? "https:" + prod.imageUrl : prod.imageUrl}
+              alt={prod.title}
+              fill
+              className="rounded-lg"
+            />
           </div>
+          <div className="flex flex-col">
+            <p className="text-white text-center mt-1">Liga Magic</p>
+            <table className="mt-1">
+              <tbody>
+                <tr>
+                  <td className="text-white">Preço mínimo</td>
+                  <td className="text-white text-right">{prod.priceMin}</td>
+                </tr>
+                <tr>
+                  <td className="text-white">Preço médio</td>
+                  <td className="text-white text-right">{prod.priceMed}</td>
+                </tr>
+                <tr>
+                  <td className="text-white">Preço máximo</td>
+                  <td className="text-white text-right">{prod.priceMax}</td>
+                </tr>
+              </tbody>
+            </table>
+            <p className="text-white text-center mt-1">Star City</p>
+            <table className="mt-1">
+              <tbody>
+                <tr>
+                  <td className="text-white">Preço mínimo</td>
+                  <td className="text-white text-right">{prod.priceMin}</td>
+                </tr>
+                <tr>
+                  <td className="text-white">Preço médio</td>
+                  <td className="text-white text-right">{prod.priceMed}</td>
+                </tr>
+                <tr>
+                  <td className="text-white">Preço máximo</td>
+                  <td className="text-white text-right">{prod.priceMax}</td>
+                </tr>
+              </tbody>
+            </table>
+            <p className="text-white text-center mt-1">Magic Domen</p>
+            <table className="mt-1">
+              <tbody>
+                <tr>
+                  <td className="text-white">Preço mínimo</td>
+                  <td className="text-white text-right">{prod.priceMin}</td>
+                </tr>
+                <tr>
+                  <td className="text-white">Preço médio</td>
+                  <td className="text-white text-right">{prod.priceMed}</td>
+                </tr>
+                <tr>
+                  <td className="text-white">Preço máximo</td>
+                  <td className="text-white text-right">{prod.priceMax}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        
         ))}
       </div>
-
-      <div ref={loadMoreRef}>Exibir</div>
     </main>
   );
 }
